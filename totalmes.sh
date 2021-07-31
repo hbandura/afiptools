21da6346f25f58b1c2795fd706afa73c207c9962
Pasto <hbandura@gmail.com>
2021-07-31 18:38:14 -0300

    Fix

--- totalmes.sh
+++ totalmes.sh
@@ -1,6 +1,6 @@
 
-DESDE=`date -v${MES}m -v${ANO}y -v1d "+%d/%m/%Y"`
-HASTA=`date -v${MES}m -v${ANO}y -v1d -v+1m -v-1d "+%d/%m/%Y"`
+export DESDE=`date -v${MES}m -v${ANO}y -v1d "+%d/%m/%Y"`
+export HASTA=`date -v${MES}m -v${ANO}y -v1d -v+1m -v-1d "+%d/%m/%Y"`
 ID=`./generar.sh | jq -r '.datos.idConsulta'`
 ID=$ID ./listar.sh | jq '[.datos.data[] | {d:.[0],c:.[14],v:.[23]} | .v | tonumber] | add'
 
